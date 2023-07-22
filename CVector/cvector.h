666353e66d7c3574c3ebe57f816803492480f301
Ahmmed Sakib Noman <nomanahmmed0@gmail.com>
2023-07-22 13:03:22 +0600

    Cvector Added

---
+++ CVector/cvector.h
@@ -0,0 +1,72 @@
+#ifndef __CVECTOR_H__
+#define __CVECTOR_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+
+typedef struct {
+    char * data;
+    size_t size, capacity, elem_size;
+} cvector ;
+
+cvector cv_create ( size_t size ) {
+    cvector cv = {
+        .data = NULL,
+        .size = 0,
+        .capacity = 0,
+        .elem_size = size
+    };
+    return cv;
+}
+
+bool allocate_if_necessary ( cvector * cv );
+size_t cv_size ( cvector * cv );
+bool cv_emplace_back ( cvector * cv, void * elem );
+void* cv_at ( cvector * cv, size_t index );
+void cv_destroy ( cvector * cv );
+
+#ifdef CVECTOR_IMPLEMENTATIONS
+bool allocate_if_necessary ( cvector * cv ) {
+    if ( cv->size == cv->capacity ) {
+        cv->capacity = (cv->capacity == 0 ? 1 : cv->capacity * 2);
+        cv->data = (char*) realloc ( cv->data, cv->capacity * cv->elem_size );
+        if ( errno == ENOMEM ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+size_t cv_size ( cvector * cv ) {
+    return cv->size;
+}
+
+bool cv_emplace_back ( cvector * cv, void * elem ) {
+    if ( !allocate_if_necessary ( cv ) ) return false;
+    memcpy ( cv->data + ( cv->size * cv->elem_size ), elem, cv->elem_size );
+    cv->size++;
+    return true;
+}
+
+void* cv_at ( cvector * cv, size_t index ) {
+    if ( index > cv->size ) {
+        fprintf ( stderr, "Indexing out of size\n" );
+        return NULL;
+    }
+    return cv->data + ( index * cv->elem_size );
+}
+
+void cv_destroy ( cvector * cv ) {
+    if ( cv->data != NULL )
+        free ( cv->data );
+    cv->data = NULL;
+    cv->size = 0;
+    cv->capacity = 0;
+    cv->elem_size = 0;
+}
+#endif // CVECTOR_IMPLEMENTATIONS
+
+#endif // __CVECTOR_H__
---
+++ CVector/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define CVECTOR_IMPLEMENTATIONS
+#include "cvector.h"
+
+int main ( int argc, char ** argv ) {
+    (void) argc; (void) argv;
+
+    cvector cv = cv_create ( sizeof ( int ) );
+
+    for ( int i = 0; i < 10; i++ ) {
+        cv_emplace_back ( &cv, &i );
+    }
+
+    for ( int i = 0; i < (int)cv_size ( &cv ); i++ ) {
+        printf ( "%d ", *(int*) cv_at ( &cv, (size_t)i ));
+    }
+
+    cv_destroy ( &cv );
+
+    return 0;
+}
